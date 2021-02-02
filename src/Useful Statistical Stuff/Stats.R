71bfdcf39bb6df10c820664975f27334eea43907
bryansashakim <70169877+bryansashakim@users.noreply.github.com>
2021-02-02 13:48:05 -0800

    Delete src/Useful Statistical Stuff directory

--- src/Useful Statistical Stuff/Stats.R
+++
@@ -1,7 +0,0 @@
-# TAB
-dataset %>%
-  group_by(var) %>%
-  summarise(n = n()) %>%
-  mutate(totalN = (cumsum(n)),
-         percent = round((n / sum(n)), 3),
-         cumpercent = round(cumsum(freq = n / sum(n)), 3))
